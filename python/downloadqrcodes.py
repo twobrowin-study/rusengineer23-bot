9f08d91f5027513dc1ab80568ef2717949fe18a8
Egor Dubrovin <dubrovin.en@ya.ru>
2023-10-30 17:20:06 +0300

    Добавлены переменные окружения для автоматизации выкачки QR кодов

--- python/downloadqrcodes.py
+++ python/downloadqrcodes.py
@@ -2,8 +2,15 @@ import pandas as pd
 import requests as r
 import base64
 
-df = pd.read_csv('skud.csv', names=['num', 'url'], delimiter=';')
+import os, dotenv
+
+dotenv.load_dotenv()
+
+CSV_NAME = os.environ.get('CSV_NAME')
+XLSX_QR_NAME = os.environ.get('XLSX_QR_NAME')
+
+df = pd.read_csv(CSV_NAME, names=['num', 'url'], delimiter=',')
 
 df['image'] = df['url'].apply(lambda url: base64.b64encode(r.get(url).content).decode())
 
-df.to_excel('output.xlsx')
+df.to_excel(XLSX_QR_NAME)
